37a0b90600657e1795104b71c9cfd4b3485310b3
Dan <14043624+delivrance@users.noreply.github.com>
2021-09-30 13:57:38 +0200

    Update test_ctr.py

--- tests/ctr/test_ctr.py
+++ tests/ctr/test_ctr.py
@@ -108,7 +108,7 @@ class TestCTR256Cryptography(unittest.TestCase):
 
 
 class TestCTR256Input(unittest.TestCase):
-    TYPE_ERROR_PATTERN = r"'\w+' does not support the buffer interface|a bytes-like object is required, not '\w+'"
+    TYPE_ERROR_PATTERN = r"'\w+' does not (support|have) the buffer interface|a bytes-like object is required, not '\w+'"
 
     def test_ctr256_encrypt_invalid_args_count(self):
         with self.assertRaisesRegex(TypeError, r"function takes exactly \d arguments \(\d given\)"):
