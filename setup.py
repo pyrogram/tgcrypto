675376dc2ea28ff593b72d856f6bf02c86924f15
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2024-11-05 20:31:43 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="TgCrypto",
-    version="1.2.5",
+    version="1.2.6",
     description="Fast and Portable Cryptography Extension Library for Pyrogram",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -45,6 +45,8 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Programming Language :: Python :: Implementation",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
